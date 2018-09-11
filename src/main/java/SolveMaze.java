42523f42e5da85ee3c508e583959ef7796247ff0
schneid2000 <bhs5@illinois.edu>
2018-09-11 18:18:35 -0500

    Implemented original version of algorithm.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,20 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                if (!maze.canMove()) {
+                    maze.turnRight();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnRight();
+                        maze.turnRight();
+                    }
+                } else {
+
+                }
+            }
         }
 
         if (maze.isFinished()) {
